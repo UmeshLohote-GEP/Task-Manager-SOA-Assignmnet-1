a83de8d45938f7c4286ac991bb0baae8142a3fd5
UmeshLohote-GEP <umeshlohote@gmail.com>
2023-11-12 00:23:11 +0530

    first commit

---
+++ TaskManager/Startup.cs
@@ -0,0 +1,48 @@
+﻿using TaskManager.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+public class Startup
+{
+    public IConfiguration configRoot
+    {
+        get;
+    }
+    public Startup(IConfiguration configuration)
+    {
+        configRoot = configuration;
+    }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        //services.AddDbContext<TaskDbContext>(options => options.UseInMemoryDatabase("TaskDatabase"));
+        //services.AddOcelot();
+    }
+    public void Configure(WebApplication app, IWebHostEnvironment env)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            //app.UseSwaggerForOcelotUI(opt => {});
+            //.UseOcelot()
+            //.Wait();
+            app.UseSwaggerForOcelotUI(opt =>
+            {
+                opt.PathToSwaggerGenerator = "/swagger/docs";
+            });
+        }
+
+
+        app.UseRouting();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+        app.UseOcelot().Wait();
+        app.Run();
+    }
+}
